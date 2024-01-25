c72cbb141e8fb27e9f05e512f2fd8399c3c30c35
carlosmandele <carlos.mandelle@gmail.com>
2024-01-25 03:17:18 +0000

    Extarção de conjuntos de itens mais frequentes

--- association_rule_extraction.R
+++ association_rule_extraction.R
@@ -21,3 +21,30 @@ itemFrequencyPlot(groceries_tr, support=0.10)
 
 # Visualização de números reais com 3 casas decimais
 options(digits=3)
+
+#----------------------------------------------#
+# EXTRAINDO CONJUNTOS DE ITENS MAIS FREQUENTES #
+#----------------------------------------------#
+
+# Número de conjuntos de itens potencialmente frequentes
+2^ncol(groceries_tr)
+
+# Extração de conjuntos de itens frequentes para minsupport = 2%
+fi <- apriori(groceries, parameter = list(supp = 0.02, target = "frequent itemsets"))
+
+# Exibindo conjuntos de itens frequentes em 'fi'
+inspect(fi)
+
+# Exibindo um histograma dos tamanhos de conjuntos de itens freqüentes em 'fi'
+barplot(table(size(fi)), xlab="Taille itemset", ylab="Nombre")
+
+# Classificação de conjuntos de itens frequentes em ordem decrescente de seu meio
+fi <- sort(fi, by="support")
+
+# Visualizando os 30 conjuntos de itens mais comuns
+inspect(head(fi, n=30))
+# Ou também
+inspect(fi[1:30,])
+
+# Exibindo uma lista de conjuntos de itens comuns com tamanho ≥ 3, ou seja, consistindo em mais de 2 itens.
+inspect(fi[size(fi)>2])
