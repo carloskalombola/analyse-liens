fa41d37873d2a3129fbeb73f9dc80f8baf5e6991
carlosmandele <carlos.mandelle@gmail.com>
2024-01-26 03:28:22 +0000

    Tecnicas de extração de regras de associação

--- association_rule_extraction.R
+++ association_rule_extraction.R
@@ -48,3 +48,34 @@ inspect(fi[1:30,])
 
 # Exibindo uma lista de conjuntos de itens comuns com tamanho ≥ 3, ou seja, consistindo em mais de 2 itens.
 inspect(fi[size(fi)>2])
+
+#--------------------------------#
+# EXTRAINDO REGRAS DE ASSOCIAÇÃO #
+#--------------------------------#
+
+# Extraindo regras de associação para minsupport = 1% e mintrust = 40%
+rules1 <- apriori(groceries, parameter = list(supp = 0.01, conf = 0.4, target = "rules"))
+inspect(rules1)
+
+# Calendarização das regras através de medidas geradoras de confiança e de apoio, respetivamente
+rules1 <- sort(rules1, by = c("confidence", "support"))
+inspect(rules1)
+
+# Regras de associação de tamanho máximo de 2 itens para minsupport = 1% e minconfidence = 40% 
+rules2 <- apriori(groceries, parameter = list(supp = 0.01, conf = 0.4, target = "rules", maxlen=2))
+inspect(rules2)
+
+# Regras de associação de tamanho máximo de 2 itens para minsupport = 0,1% e minconfidence = 50%
+rules3 <- apriori(groceries, parameter = list(supp = 0.001, conf = 0.5, target = "rules", maxlen=2))
+rules3 <- sort(rules3, by=c("confidence", "support"))
+inspect(rules3)
+
+# Segmentação do item 'Whole.milk=Y' na história do período
+wm1 <- apriori(data=groceries, parameter=list(supp=0.01, conf=0.2), appearance=list(lhs="Whole.milk=Y"), control=list(verbose=F))
+wm1 <- sort(wm1, by = c("confidence", "support"))
+inspect(wm1)
+
+# Direcionamento do item 'Whole.milk=Y' como resultado das regras
+wm2 <- apriori(data=groceries, parameter=list(supp=0.01, conf=0.55), appearance=list(rhs="Whole.milk=Y"), control=list(verbose=F))
+wm2 <- sort(wm2, by = c("confidence", "support"))
+inspect(wm2)
